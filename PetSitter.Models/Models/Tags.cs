a8866f6fff4169f5b5dbc75ec82169fc67c2a6ff
kleqing <nhanpk28122004@gmail.com>
2025-09-14 22:29:53 +0700

    [Update] Add check null

--- PetSitter.Models/Models/Tags.cs
+++ PetSitter.Models/Models/Tags.cs
@@ -3,8 +3,8 @@ namespace PetSitter.Models.Models;
 public class Tags
 {
     public Guid TagId { get; set; }
-    public Guid ProductId { get; set; }
-    public Guid BlogId { get; set; }
+    public Guid? ProductId { get; set; }
+    public Guid? BlogId { get; set; }
     public string TagName { get; set; } = string.Empty;
 
     public virtual ICollection<Products> Products { get; set; } = new List<Products>();
