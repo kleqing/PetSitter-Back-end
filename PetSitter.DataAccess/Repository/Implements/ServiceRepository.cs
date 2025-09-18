35469955cb7e4bd38d82fcc9ab6c63848cdbab56
kleqing <nhanpk28122004@gmail.com>
2025-09-18 20:54:13 +0700

    [Add] Add service

---
+++ PetSitter.DataAccess/Repository/Implements/ServiceRepository.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.EntityFrameworkCore;
+using PetSitter.DataAccess.Repository.Interfaces;
+using PetSitter.Models.Models;
+
+namespace PetSitter.DataAccess.Repository.Implements;
+
+public class ServiceRepository : IServiceRepository
+{
+    private readonly ApplicationDbContext _context;
+    
+    public ServiceRepository(ApplicationDbContext context)
+    {
+        _context = context;
+    }
+    
+    public async Task<List<Services>> ListAllServices()
+    {
+        var service=  await _context.Services.Include(x => x.ServiceReviews).ToListAsync();
+        return service;
+    }
+    
+    public async Task<Services?> RetrieveServiceFromId(Guid serviceId)
+    {
+        return await _context.Services
+            .Include(x => x.ServiceReviews)
+            .Include(x => x.Shop)
+            .FirstOrDefaultAsync(x => x.ServiceId == serviceId);
+    }
+
+    public async Task<List<ServiceReview>> RetrieveServiceReviewsByServiceId(Guid serviceId)
+    {
+        var service = await _context.ServiceReviews
+            .Include(x => x.Users)
+            .Where(x => x.ServiceId == serviceId)
+            .ToListAsync();
+        return service;
+    }
+}
---
+++ PetSitter.DataAccess/Repository/Interfaces/IServiceRepository.cs
@@ -0,0 +1,10 @@
+﻿using PetSitter.Models.Models;
+
+namespace PetSitter.DataAccess.Repository.Interfaces;
+
+public interface IServiceRepository
+{
+    Task<List<Services>> ListAllServices();
+    Task<Services?> RetrieveServiceFromId(Guid serviceId);
+    Task<List<ServiceReview>> RetrieveServiceReviewsByServiceId(Guid serviceId);
+}
